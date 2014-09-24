25f9d12d17071916f10e9e94e9047365ae20e09a
Sinuhe MB <gnu.ico@gmail.com>
2014-09-24 13:09:48 -0500

    Agrega lib de js para eventos de iframe
    Signed-off-by: Sinuhe MB <gnu.ico@gmail.com>

--- dashboard-web/src/main/webapp/WEB-INF/public/js/dashboard.js
+++ dashboard-web/src/main/webapp/WEB-INF/public/js/dashboard.js
@@ -3,54 +3,22 @@ function onMain(url) {
 }
 
 $(function() {
-	
+
 	/*
-	 * Script para esconder los "menu items" de "menubar" de primefaces usando iframe
+	 * Script para esconder los "menu items" de "menubar" de primefaces usando
+	 * iframe
 	 */
 	$('#f_menubar .ui-menu-list').hover(function() {
 	}, function() {
 		$('#f_menubar .ui-menu-child').hide();
 		$('#f_menubar .ui-menu-list li').removeClass('ui-menuitem-active');
-		$('#f_menubar .ui-menu-list li a').removeClass('ui-state-hover');		
+		$('#f_menubar .ui-menu-list li a').removeClass('ui-state-hover');
 	});
-	
-	$('#f_menubar\\:menu_user ul').hover(function() {
-	}, function() {		
-		$('#f_menubar\\:menu_user_menu').css('display','none');		
-		$('#f_menubar\\:menu_user_button').blur();
+
+	$('#main').iframeTracker({
+		blurCallback : function() {
+			$('#f_menubar\\:menu_user_menu').hide();
+		}
 	});
-	
-	
-	
-	$('#f_menubar\\:user').click(function() {
 
-		$('#main').attr('src', 'http://www.w3schools.com/tags/tag_br.asp')
-
-		// $('#main').html("s");
-
-		// $.ajax({
-		// url : "http://dev:8080/web/login",
-		// crossDomain : true
-		// }).done(function(data) {
-		// $('#main').html(data);
-		// });
-
-	})
-
-	
-//	var iframeDoc = $('#main').contents().get(0);
-//	$(iframeDoc).bind('click', function( event ) {
-//		$('.ui-menu-child').hide()
-//	});
-//	
-	
-	
-	$('#main').hover(function () {		
-			//$('.ui-menu-child').hide();		
-	}, function () {
-		//$('.ui-menu-child').css('display','block');
-	});
-	
-	
-	
 });
