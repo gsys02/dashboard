f78e12f2f3134b601f232a146c88e1cbb0762f5c
Sinuhe MB <gnu.ico@gmail.com>
2014-09-24 18:38:35 -0500

    agregar termino de session
    Signed-off-by: Sinuhe MB <gnu.ico@gmail.com>

---
+++ dashboard-web/src/main/java/sys02/web/logout/Logout.java
@@ -0,0 +1,22 @@
+package sys02.web.logout;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Logout {
+
+	@RequestMapping("/logout")
+	public String logout(HttpServletRequest request,
+			HttpServletResponse response, HttpSession session) {
+
+		session.invalidate();
+		
+		return "redirect:https://gnextia.com/cas/logout";
+	}
+}
