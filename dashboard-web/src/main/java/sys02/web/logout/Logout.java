09fb434ab096420d94497f82113a4487569ccdf7
Sinuhe MB <gnu.ico@gmail.com>
2014-09-24 20:14:22 -0500

    subir logout
    Signed-off-by: Sinuhe MB <gnu.ico@gmail.com>

--- dashboard-web/src/main/java/sys02/web/logout/Logout.java
+++ dashboard-web/src/main/java/sys02/web/logout/Logout.java
@@ -5,18 +5,38 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.jasig.cas.client.session.SingleSignOutHandler;
+import org.pac4j.cas.client.CasClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+//@Controller
 public class Logout {
 
-	@RequestMapping("/logout")
+	
+
+	
+	//@RequestMapping("/logout")
 	public String logout(HttpServletRequest request,
 			HttpServletResponse response, HttpSession session) {
 
-		session.invalidate();
 		
+		
+		
+		/*
+		session.invalidate();
+
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null)
+			for (int i = 0; i < cookies.length; i++) {
+				cookies[i].setValue("");
+				cookies[i].setPath("/");
+				cookies[i].setMaxAge(0);
+				response.addCookie(cookies[i]);
+			}
+			*/
+
 		return "redirect:https://gnextia.com/cas/logout";
 	}
 }
